927e57469a2795703dbdba1b3b641b627675bf85
ekazantsev <ekazantsev@ocs.ru>
2023-10-24 23:21:34 +0400

    изменены зависимости

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ repositories {
 dependencies {
     api("org.apache.httpcomponents:httpclient:4.5.14")
     api("org.apache.httpcomponents:httpmime:4.5.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    api("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("org.slf4j:slf4j-api:2.0.9")
 
     testImplementation("ch.qos.logback:logback-classic:1.4.11")
